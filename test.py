4ca0cee1fb7a233a0ba3cbcce496e4cf0f6679fe
ktanguy <144002903+ktanguy@users.noreply.github.com>
2025-01-10 18:20:17 +0200

    Recreate the function with the sigmoid Function ,  use the post decorator

--- test.py
+++ test.py
@@ -4,22 +4,26 @@ import numpy as np
 
 app = FastAPI()
 
+
+
 # use the post decorator directly below this
-'''
-    Initialize M and B as np arrays
-'''
+@app.post("/calculate")
 def f(x):
     pass
  
 #Implement the formula MX + B
+
 #Have two function one using numpy and another not using numpy
 #Return 
 
 #initialize x as a 5 * 5 matrix
 
+
 #Make a call to the function
 
 #Recreate the function with the sigmoid Function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 if __name__ == "__main__":
     uvicorn.run(app)
