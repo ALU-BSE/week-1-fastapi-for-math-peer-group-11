fc6d71a0c1193509b251bd5cb936c513f1c539a4
Kanisa1 <k.thiak@alustudent.com>
2025-01-10 22:24:53 +0200

    intial

--- test.py
+++ test.py
@@ -10,6 +10,18 @@ class MatrixInput(BaseModel):
     matrix: list[list[float]]
 
 # Predefined matrices
+M = np.array([[1, 2, 3, 4, 5],
+              [6, 7, 8, 9, 10],
+              [11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20],
+              [21, 22, 23, 24, 25]])
+
+B = np.array([[0, 2, 4, 6, 8],
+              [1, 3, 5, 7, 9],
+              [2, 4, 6, 8, 10],
+              [3, 5, 7, 9, 11],
+              [4, 6, 8, 10, 12]])
+
 
 
 # use the post decorator directly below this
